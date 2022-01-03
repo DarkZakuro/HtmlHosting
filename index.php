d813fe162b35872a9a96635a64aa8c45c6523d87
Samar <samarpclenevo@gmail.com>
2022-01-03 14:19:22 +0530

    Initial Commit

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-include 'ip.php';
-#Replace CSS ASSIGNMENT/CSS PRACTICAL ASSIGNMENT.html with your html file.
-header('Location: https://6983-192-140-153-245.ngrok.io/CSS ASSIGNMENT/CSS PRACTICAL ASSIGNMENT.html');
-exit
-?>
